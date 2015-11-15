86360f9f93d2fb097989fb6100178a5be2a62092
adanac <adanac@sina.com>
2015-11-15 20:27:56 +0800

    update by adanac

--- src/com/adanac/eduapp/MainActivity.java
+++ src/com/adanac/eduapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.adanac.eduapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
